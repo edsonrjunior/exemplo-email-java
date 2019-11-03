41167f131d9c1d38f9b35b8a2987288efc3bcc62
Hugo <hodestito@gmail.com>
2019-11-02 21:32:25 -0300

    Acerto em variaveis

--- src/main/java/br/com/fiap/g1/eventos/EventosComunicacaoApplication.java
+++ src/main/java/br/com/fiap/g1/eventos/EventosComunicacaoApplication.java
@@ -1,13 +1,14 @@
-package br.com.fiap.g1.eventos.comunicacao;
+package br.com.fiap.g1.eventos;
 
 import java.util.Arrays;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import br.com.fiap.g1.eventos.envio.Mailer;
 import br.com.fiap.g1.eventos.envio.Mensagem;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class EventosComunicacaoApplication {
 
 	public static void main(String[] args) {
@@ -15,7 +16,7 @@ public class EventosComunicacaoApplication {
 //		SpringApplication.run(EventosComunicacaoApplication.class, args);
 
 		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
-				EventosComunicacaoApplication.class);
+				EventosComunicacaoApplication.class.getPackage().getName());
 
 		Mailer mailer = applicationContext.getBean(Mailer.class);
 
